089ab23d3e930a328b59add0a642c1211214cd3a
harshit350 <74818234+harshit350@users.noreply.github.com>
2021-01-14 22:35:05 +0530

    Add files via upload

---
+++ SLINGSHOT.js
@@ -0,0 +1,34 @@
+class SlingShot{
+    constructor(bodyA,pointB){
+         var options={
+
+        bodyA:bodyA,
+        pointB:pointB,
+        stiffness:0.05,
+        length:5
+    }
+    this.pointB=pointB
+    this.sling=Constraint.create(options);
+World.add(world,this.sling);
+    }
+
+    fly(){
+        this.sling.bodyA=null;
+
+    }
+display(){
+if(this.sling.bodyA){
+    
+var pointA=this.sling.bodyA.position;
+var pointB=this.pointB;
+
+
+stroke("yellow");
+strokeWeight(5);
+line(pointA.x,pointA.y,pointB.x,pointB.y);
+
+}
+
+}
+
+}
